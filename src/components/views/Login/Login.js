8e6efaef3f280dee3046934b43d847a52242c056
Marianna Sowinska <marianna.sowi@gmail.com>
2020-12-08 13:16:28 +0100

    Fix 22.1 and 22.2

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import styles from './Login.module.scss';
-import { Paper } from '@material-ui/core';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
+import styles from './Login.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const Login = () => (
-  <Paper className={styles.component}>
-    <TableHead>Login view
-      <TableRow>
-        <TableCell>ID number</TableCell>
-        <TableCell><input></input></TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell>Pasword</TableCell>
-        <TableCell><input></input></TableCell> 
-      </TableRow>
-    </TableHead>
-    <Button component={Link} to={`${process.env.PUBLIC_URL}/`}>Log in</Button>
-  </Paper>
+  <div className={styles.component}>
+    <h2>Login</h2>
+    <Container maxWidth='lg'>
+      <form className={styles.form} noValidate autoComplete='off'>
+        <TextField
+          className={styles.input}
+          id="outlined-secondary"
+          label="Login"
+          variant="outlined"
+          color="secondary"
+        />
+        <TextField
+          className={styles.input}
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          color="secondary"
+        />
+        <Button className={styles.button} size='large' variant="contained" color="primary" component={NavLink} exact to={`${process.env.PUBLIC_URL}/`}>
+          Login
+        </Button>
+      </form>
+    </Container>
+  </div>
 );
 
 export default Login;
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './Waiter.module.scss';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,129 +8,84 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 
-class Waiter extends React.Component {
-  static propTypes = {
-    fetchTables: PropTypes.func,
-    tables: PropTypes.array,
-    loading: PropTypes.shape({
-      active: PropTypes.bool,
-      error: PropTypes.oneOfType([PropTypes.bool,PropTypes.string]),
-    }),
-    changedStatus: PropTypes.func,
-  }
+const demoContent = [
+  {id: '1', status: 'free', order: null},
+  {id: '2', status: 'thinking', order: null},
+  {id: '3', status: 'ordered', order: 123},
+  {id: '4', status: 'prepared', order: 234},
+  {id: '5', status: 'delivered', order: 345},
+  {id: '6', status: 'paid', order: 456},
+];
 
-  componentDidMount(){
-    const { fetchTables } = this.props;
-    fetchTables();
-  }
-
-  tableStatusChange(row) {
-    if (row.status === 'free') {
-      row.status = 'ordered';
-    }
-    else if (row.status === 'ordered') {
-      row.status = 'prepared';
-    }
-    else if (row.status === 'prepared') {
-      row.status = 'delivered';
-    }
-    else if (row.status === 'delivered') {
-      row.status = 'paid';
-    }
-    else if (row.status === 'paid') {
-      row.status = 'free';
-    }
-
-    const {changedStatus} = this.props;
-    changedStatus(row);
-  }
-
-  renderActions(row){
-    const status = row.status;
-    switch (status) {
-      case 'free':
-        return (
-          <>
-            <Button onClick={()=> this.tableStatusChange(row)}>new order</Button>
-          </>
-        );
-      case 'ordered':
-        return (
-          <Button onClick={()=>this.tableStatusChange(row)}>prepared</Button>
-        );
-        
-      case 'prepared':
-        return (
-          <Button onClick={()=>this.tableStatusChange(row)}>delivered</Button>
-        );
-      case 'delivered':
-        return (
-          <Button onClick={()=>this.tableStatusChange(row)}>paid</Button>
-        );
-      case 'paid':
-        return (
-          <Button onClick={this.tableStatusChange(row)}>free</Button>
-        );
-      default:
-        return null;
-    }
-  }
-
-  render() {
-    const { loading: { active, error }, tables } = this.props;
-
-    if(active || !tables.length){
+const renderActions = status => {
+  switch (status) {
+    case 'free':
       return (
-        <Paper className={styles.component}>
-          <p>Loading...</p>
-        </Paper>
+        <>
+          <Button>thinking</Button>
+          <Button>new order</Button>
+        </>
       );
-    } else if(error) {
+    case 'thinking':
       return (
-        <Paper className={styles.component}>
-          <p>Error! Details:</p>
-          <pre>{error}</pre>
-        </Paper>
+        <Button>new order</Button>
       );
-    } else {
+    case 'ordered':
       return (
-        <Paper className={styles.component}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Table</TableCell>
-                <TableCell>Status</TableCell>
-                <TableCell>Order</TableCell>
-                <TableCell>Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {tables.map(row => (
-                <TableRow key={row.id}>
-                  <TableCell component="th" scope="row">
-                    {row.id}
-                  </TableCell>
-                  <TableCell>
-                    {row.status}
-                  </TableCell>
-                  <TableCell>
-                    {row.order && (
-                      <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
-                        {row.order}
-                      </Button>
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    {this.renderActions(row)}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Paper>
+        <Button>prepared</Button>
       );
-    }
+    case 'prepared':
+      return (
+        <Button>delivered</Button>
+      );
+    case 'delivered':
+      return (
+        <Button>paid</Button>
+      );
+    case 'paid':
+      return (
+        <Button>free</Button>
+      );
+    default:
+      return null;
   }
-}
+};
+
+const Waiter = () => (
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Table</TableCell>
+          <TableCell>Status</TableCell>
+          <TableCell>Order</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {demoContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell>
+              {row.status}
+            </TableCell>
+            <TableCell>
+              {row.order && (
+                <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                  {row.order}
+                </Button>
+              )}
+            </TableCell>
+            <TableCell>
+              {renderActions(row.status)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Paper>
+);
 
 export default Waiter;
